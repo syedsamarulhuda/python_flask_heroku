54a39b2d752392505d82ebda996081e1956428b2
Syed Samarul Huda <syed.samarul.huda@flyrobe.com>
2017-07-01 19:36:45 +0530

    change in app.py file

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-import os
- 
+
 from flask import Flask
+		
 app = Flask(__name__)
+		
 @app.route('/')
 def index():
-return'<h1>Hello, Flask! We are deploying</h1>'
+	return'<h1>Hello, Flask! We are deploying</h1>'
+		
 if __name__ == '__main__':
-app.run(debug=True)
+	app.run(debug=True)
